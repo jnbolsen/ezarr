e0c9e7ba782db4eae827d5f4d973c351e55b1872
jnbolsen <61256624+jnbolsen@users.noreply.github.com>
2022-11-17 10:46:45 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,38 +1,44 @@
 #!/bin/bash
 
 # Make users and group
-sudo useradd sonarr -u 13001
-sudo useradd radarr -u 13002
-sudo useradd lidarr -u 13003
-sudo useradd readarr -u 13004
-sudo useradd mylar -u 13005
+sudo useradd radarr1 -u 13001
+sudo useradd radarr2 -u 13002
+sudo useradd sonarr -u 13003
+sudo useradd lidarr -u 13004
+sudo useradd readarr -u 13005
 sudo useradd prowlarr -u 13006
-sudo useradd qbittorrent -u 13007
-sudo useradd jackett -u 13008
+sudo useradd bazarr1 -u 13007
+sudo useradd bazarr2 -u 13008
+sudo useradd qbittorrent -u 13009
+sudo useradd overseerr -u 13010
 sudo groupadd mediacenter -g 13000
+sudo usermod -a -G mediacenter radarr1
+sudo usermod -a -G mediacenter radarr2
 sudo usermod -a -G mediacenter sonarr
-sudo usermod -a -G mediacenter radarr
 sudo usermod -a -G mediacenter lidarr
 sudo usermod -a -G mediacenter readarr
-sudo usermod -a -G mediacenter mylar
 sudo usermod -a -G mediacenter prowlarr
+sudo usermod -a -G mediacenter bazarr1
+sudo usermod -a -G mediacenter bazarr2
 sudo usermod -a -G mediacenter qbittorrent
-sudo usermod -a -G mediacenter jackett
+sudo usermod -a -G mediacenter overseerr
 
 # Make directories
 sudo mkdir -pv docker/{sonarr,radarr,lidarr,readarr,mylar,prowlarr,qbittorrent,jackett}-config
-sudo mkdir -pv data/{torrents,media}/{tv,movies,music,books,comics}
+# Need to mount network share to unraid or truenas VM
 
 # Set permissions
 sudo chmod -R 775 data/
 sudo chown -R $(id -u):mediacenter data/
+sudo chown -R radarr:mediacenter docker/radarr1-config
+sudo chown -R radarr:mediacenter docker/radarr2-config
 sudo chown -R sonarr:mediacenter docker/sonarr-config
-sudo chown -R radarr:mediacenter docker/radarr-config
 sudo chown -R lidarr:mediacenter docker/lidarr-config
 sudo chown -R readarr:mediacenter docker/readarr-config
-sudo chown -R mylar:mediacenter docker/mylar-config
 sudo chown -R prowlarr:mediacenter docker/prowlarr-config
+sudo chown -R radarr:mediacenter docker/bazarr1-config
+sudo chown -R radarr:mediacenter docker/bazarr2-config
 sudo chown -R qbittorrent:mediacenter docker/qbittorrent-config
-sudo chown -R jackett:mediacenter docker/jackett-config
+sudo chown -R jackett:mediacenter docker/overseerr-config
 
 echo "UID=$(id -u)" >> .env
