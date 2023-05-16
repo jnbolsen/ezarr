a82a68d9bf79797a5029d9617385fcc9c16e93ca
jnbolsen <61256624+jnbolsen@users.noreply.github.com>
2023-05-16 08:55:36 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
 # Make users and group
-sudo useradd radarr1 -u 3001
-sudo useradd radarr2 -u 3002
-sudo useradd sonarr -u 3003
-sudo useradd lidarr -u 3004
-sudo useradd readarr -u 3005
-sudo useradd prowlarr -u 3006
-sudo useradd bazarr1 -u 3007
-sudo useradd bazarr2 -u 3008
-sudo useradd qbittorrent -u 3009
-sudo useradd nzbget -u 3010
-sudo useradd overseerr -u 3011
+sudo useradd radarr -u 13011
+sudo useradd sonarr -u 13002
+sudo useradd lidarr -u 13003
+sudo useradd readarr -u 13004
+sudo useradd prowlarr -u 13005
+sudo useradd bazarr -u 13006
+sudo useradd qbittorrent -u 13007
+sudo useradd nzbget -u 13008
+sudo useradd overseerr -u 13009
+sudo useradd notifiarr -u 13010
 sudo groupadd mediacenter -g 13000
-sudo usermod -a -G mediacenter radarr1
-sudo usermod -a -G mediacenter radarr2
-sudo usermod -a -G mediacenter sonarr
-sudo usermod -a -G mediacenter lidarr
-sudo usermod -a -G mediacenter readarr
-sudo usermod -a -G mediacenter prowlarr
-sudo usermod -a -G mediacenter bazarr1
-sudo usermod -a -G mediacenter bazarr2
-sudo usermod -a -G mediacenter qbittorrent
-sudo usermod -a -G mediacenter nzbget
-sudo usermod -a -G mediacenter overseerr
+sudo usermod -aG mediacenter radarr
+sudo usermod -aG mediacenter sonarr
+sudo usermod -aG mediacenter lidarr
+sudo usermod -aG mediacenter readarr
+sudo usermod -aG mediacenter prowlarr
+sudo usermod -aG mediacenter bazarr
+sudo usermod -aG mediacenter qbittorrent
+sudo usermod -aG mediacenter nzbget
+sudo usermod -aG mediacenter overseerr
+sudo usermod -aG mediacenter notifiarr
 
 echo "UID=$(id -u)" >> .env
