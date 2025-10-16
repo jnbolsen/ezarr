b105fa675fbe2fb854e069582f70fd706e3f4d29
jnbolsen <61256624+jnbolsen@users.noreply.github.com>
2025-10-15 21:34:01 -0500

    Update setup.sh
    remove sabnzb user

--- setup.sh
+++ setup.sh
@@ -10,7 +10,6 @@ sudo useradd tautulli -u 13004
 sudo useradd prowlarr -u 13005
 sudo useradd bazarr -u 13006
 sudo useradd qbittorrent -u 13007
-sudo useradd sabnzbd -u 13012
 sudo useradd overseerr -u 13009
 sudo useradd notifiarr -u 13010
 sudo groupadd mediacenter -g 13000
@@ -29,7 +28,6 @@ sudo usermod -aG mediacenter tautulli
 sudo usermod -aG mediacenter prowlarr
 sudo usermod -aG mediacenter bazarr
 sudo usermod -aG mediacenter qbittorrent
-sudo usermod -aG mediacenter sabnzbd
 sudo usermod -aG mediacenter overseerr
 sudo usermod -aG mediacenter notifiarr
 sudo usermod -aG mediacenter $USER
