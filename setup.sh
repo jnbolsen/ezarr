48f9593e8e87a52343c9731d8fecd0b246543ebd
jnbolsen <61256624+jnbolsen@users.noreply.github.com>
2025-11-24 20:31:50 -0600

    Update setup.sh
    - Add user slskd
    - Change docker volume location

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Make users and group
-# DO NOT USE 13008. That is used for immich on another server.
+# DO NOT USE 13001. That is used for immich on another server.
 sudo groupadd mediacenter -g 13000
 sudo useradd media -u 13000 -g mediacenter
 sudo useradd radarr -u 13011
@@ -11,15 +11,16 @@ sudo useradd tautulli -u 13004
 sudo useradd prowlarr -u 13005
 sudo useradd bazarr -u 13006
 sudo useradd qbittorrent -u 13007
+sudo useradd slskd -u 13008
 sudo useradd overseerr -u 13009
 sudo useradd notifiarr -u 13010
 
 # Make directories
-sudo mkdir -pv /home/$USER/docker-config
+sudo mkdir -pv ./volumes
 
 # Set permissions
-sudo chmod -R 775 /home/$USER/docker-config
-sudo chown -R media:mediacenter /home/$USER/docker-config
+sudo chmod -R 775 ./volumes
+sudo chown -R media:mediacenter ./volumes
 sudo usermod -aG mediacenter radarr
 sudo usermod -aG mediacenter sonarr
 sudo usermod -aG mediacenter lidarr
@@ -27,6 +28,7 @@ sudo usermod -aG mediacenter tautulli
 sudo usermod -aG mediacenter prowlarr
 sudo usermod -aG mediacenter bazarr
 sudo usermod -aG mediacenter qbittorrent
+sudo usermod -aG mediacenter slskd
 sudo usermod -aG mediacenter overseerr
 sudo usermod -aG mediacenter notifiarr
 sudo usermod -aG mediacenter $USER
