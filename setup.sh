8cb647c8ea1661b9390a997b632cb486ad4d02db
JeuTheIdit <61256624+JeuTheIdit@users.noreply.github.com>
2026-01-05 13:01:15 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -35,62 +35,65 @@ print_banner() {
 
 # Create users
 # DO NOT USE 13001. That is used for immich on another server.
-# User and group mediacenter is mapped to my NAS.
-sudo useradd mediacenter -M -u 13000
-sudo useradd radarr -M -u 13011
-sudo useradd sonarr -M -u 13002
-sudo useradd lidarr -M -u 13003
-sudo useradd tautulli -M -u 13004
-sudo useradd prowlarr -M -u 13005
-sudo useradd bazarr -M -u 13006
-sudo useradd qbittorrent -M -u 13007
-sudo useradd slskd -M -u 13008
-sudo useradd seerr -M -u 13009
-sudo useradd notifiarr -M -u 13010
+# User mediacenter and group mediacenter is mapped to my NAS with permissions set.
+create_users() {
+  sudo useradd mediacenter -M -u 13000
+  sudo useradd radarr -M -u 13011
+  sudo useradd sonarr -M -u 13002
+  sudo useradd lidarr -M -u 13003
+  sudo useradd tautulli -M -u 13004
+  sudo useradd prowlarr -M -u 13005
+  sudo useradd bazarr -M -u 13006
+  sudo useradd qbittorrent -M -u 13007
+  sudo useradd slskd -M -u 13008
+  sudo useradd seerr -M -u 13009
+  sudo useradd notifiarr -M -u 13010
+  
+  print_info "Users and mediacenter group created..."
+}
 
-print_info "Users and mediacenter group created..."
-
-# Make config directory
-# NFS data directories should be set up manually.
-sudo mkdir -pv ${CONFIG_DIR}
-
-print_info "Configuration directory created..."
+# Create config directories
+# NFS data directories should be set up manually using fstab.
+config_directories() {
+  sudo mkdir -pv ${CONFIG_DIR}/{radarr,sonarr,lidarr,tautulli,prowlarr,bazarr,qbittorrent,qbitmanage,cross-seed,slskd,seerr,notifiarr}
+  
+  print_info "Configuration directories created..."
+}
 
 # Add users to mediacenter group
-sudo usermod -aG mediacenter radarr
-sudo usermod -aG mediacenter sonarr
-sudo usermod -aG mediacenter lidarr
-sudo usermod -aG mediacenter tautulli
-sudo usermod -aG mediacenter prowlarr
-sudo usermod -aG mediacenter bazarr
-sudo usermod -aG mediacenter qbittorrent
-sudo usermod -aG mediacenter slskd
-sudo usermod -aG mediacenter seerr
-sudo usermod -aG mediacenter notifiarr
-
-print_info "Users added to mediacenter group..."
-
-# Make directories
-sudo mkdir -pv ${CONFIG_DIR}/{radarr,sonarr,lidarr,tautulli,prowlarr,bazarr,qbittorrent,qbitmanage,cross-seed,slskd,seerr,notifiarr}
-
-print_info "User configuration directories created..."
+add_users() {
+  sudo usermod -aG mediacenter radarr
+  sudo usermod -aG mediacenter sonarr
+  sudo usermod -aG mediacenter lidarr
+  sudo usermod -aG mediacenter tautulli
+  sudo usermod -aG mediacenter prowlarr
+  sudo usermod -aG mediacenter bazarr
+  sudo usermod -aG mediacenter qbittorrent
+  sudo usermod -aG mediacenter slskd
+  sudo usermod -aG mediacenter seerr
+  sudo usermod -aG mediacenter notifiarr
+  
+  print_info "Users added to mediacenter group..."
+}
 
 # Set permissions
-sudo chmod -R 775 ${CONFIG_DIR}
-sudo chown -R mediacenter:mediacenter ${CONFIG_DIR}
-sudo chown -R radarr:mediacenter ${CONFIG_DIR}/radarr
-sudo chown -R sonarr:mediacenter ${CONFIG_DIR}/sonarr
-sudo chown -R lidarr:mediacenter ${CONFIG_DIR}/lidarr
-sudo chown -R tautulli:mediacenter ${CONFIG_DIR}/tautulli
-sudo chown -R prowlarr:mediacenter ${CONFIG_DIR}/prowlarr
-sudo chown -R bazarr:mediacenter ${CONFIG_DIR}/bazarr
-sudo chown -R qbittorrent:mediacenter ${CONFIG_DIR}/qbittorrent
-sudo chown -R qbittorrent:mediacenter ${CONFIG_DIR}/qbitmanage
-sudo chown -R qbittorrent:mediacenter ${CONFIG_DIR}/cross-seed
-sudo chown -R slskd:mediacenter ${CONFIG_DIR}/slskd
-sudo chown -R overseerr:mediacenter ${CONFIG_DIR}/seerr
-sudo chown -R notifiarr:mediacenter ${CONFIG_DIR}/notifiarr
+set_permissions() {
+  sudo chmod -R 775 ${CONFIG_DIR}
+  sudo chown -R mediacenter:mediacenter ${CONFIG_DIR}
+  sudo chown -R radarr:mediacenter ${CONFIG_DIR}/radarr
+  sudo chown -R sonarr:mediacenter ${CONFIG_DIR}/sonarr
+  sudo chown -R lidarr:mediacenter ${CONFIG_DIR}/lidarr
+  sudo chown -R tautulli:mediacenter ${CONFIG_DIR}/tautulli
+  sudo chown -R prowlarr:mediacenter ${CONFIG_DIR}/prowlarr
+  sudo chown -R bazarr:mediacenter ${CONFIG_DIR}/bazarr
+  sudo chown -R qbittorrent:mediacenter ${CONFIG_DIR}/qbittorrent
+  sudo chown -R qbittorrent:mediacenter ${CONFIG_DIR}/qbitmanage
+  sudo chown -R qbittorrent:mediacenter ${CONFIG_DIR}/cross-seed
+  sudo chown -R slskd:mediacenter ${CONFIG_DIR}/slskd
+  sudo chown -R overseerr:mediacenter ${CONFIG_DIR}/seerr
+  sudo chown -R notifiarr:mediacenter ${CONFIG_DIR}/notifiarr
 
-print_info "Permissions set..."
+  print_info "Permissions set..."
+}
 
 print_success "Done! It is recommended to reboot now."
